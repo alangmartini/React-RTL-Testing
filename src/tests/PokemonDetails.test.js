f9004f0ceb2f7fcd289e720b055e2052e40f3e44
gmaralan <gmartinialan@gmail.com>
2022-11-22 18:02:59 -0400

    finished req 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -7,10 +7,10 @@ import renderWithRouter from '../renderWithRouter';
 import pokemonList from '../data';
 
 describe('Tests Pokemon Details page', () => {
+  const TEST_URL = '/pokemon/25';
   test('If detailed infos about the pokemon is displayed', async () => {
     const { history } = renderWithRouter(<App />);
-    act(() => history.push('/pokemon/25'));
-    console.log(history.location.pathname);
+    act(() => history.push(TEST_URL));
 
     const heading = await screen.findByRole('heading', { name: /Details/ });
     expect(heading).toBeInTheDocument();
@@ -19,8 +19,37 @@ describe('Tests Pokemon Details page', () => {
     expect(links.some((link) => link.href.includes('/pokemon/25'))).toBeFalsy();
 
     const summary = screen.getByRole('heading', { name: /Summary/ });
-    const summaryText = screen.getByText(pokemonList[0].summary);
+    expect(summary).toBeInTheDocument();
 
-    
+    const summaryText = screen.getByText(pokemonList[0].summary);
+    expect(summaryText).toBeInTheDocument();
+  });
+  test('If there is a map sections', async () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => history.push(TEST_URL));
+
+    const mapsHeading = await screen.findByRole('heading', { name: /Game Locations/ });
+    expect(mapsHeading).toBeInTheDocument();
+
+    pokemonList[0].foundAt.forEach((location) => {
+      const images = screen.getAllByRole('img');
+      const locationName = screen.getByText(location.location);
+      expect(images.some((img) => img.src === location.map)).toBeTruthy();
+      expect(images.some((img) => img.alt.includes('location'))).toBeTruthy();
+      expect(locationName).toBeInTheDocument();
+    });
+  });
+  test('If FavoriteInput is working properly', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => history.push(TEST_URL));
+
+    const heading = screen.getByLabelText('Pokémon favoritado?');
+    expect(heading).toBeInTheDocument();
+
+    userEvent.click(heading);
+    expect(heading.checked).toBeTruthy();
+
+    userEvent.click(heading);
+    expect(heading.checked).toBeFalsy();
   });
 });
