c05fb03d04adc2791475b46128cfaf2d26ea7a52
gmaralan <gmartinialan@gmail.com>
2022-11-22 16:50:31 -0400

    middle of req 7

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen, render } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Pokemon from '../components/Pokemon';
 import renderWithRouter from '../renderWithRouter';
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { act } from 'react-dom/test-utils';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+import pokemonList from '../data';
+
+describe('Tests Pokemon Details page', () => {
+  test('If detailed infos about the pokemon is displayed', async () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => history.push('/pokemon/25'));
+    console.log(history.location.pathname);
+
+    const heading = await screen.findByRole('heading', { name: /Details/ });
+    expect(heading).toBeInTheDocument();
+
+    const links = screen.getAllByRole('link');
+    expect(links.some((link) => link.href.includes('/pokemon/25'))).toBeFalsy();
+
+    const summary = screen.getByRole('heading', { name: /Summary/ });
+    const summaryText = screen.getByText(pokemonList[0].summary);
+
+    
+  });
+});
