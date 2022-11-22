96cf3ea0ac157e057c526d83d0db64801cfda37d
gmaralan <gmartinialan@gmail.com>
2022-11-22 15:06:26 -0400

    finished req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+const imgOfNotFound = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+describe('Tests Not Found page', () => {
+  beforeEach(() => {
+    render(<NotFound />);
+  });
+
+  test('If page has a heading with text "Page requested not found"', () => {
+    const heading = screen.getByRole('heading', { name: /Page requested not found/ });
+    expect(heading).toBeInTheDocument();
+  });
+  test('If page shows proper image', () => {
+    const image = screen.getByRole('img');
+    expect(image.src).toBe(imgOfNotFound);
+  });
+});
